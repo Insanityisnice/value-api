7aaeca0fd21974f9c12c5c78de90cd43fc8f6eb6
Ganesh.Anthati <ganeshanthati@gmail.com>
2016-12-26 03:57:06 +0530

    updated function method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const mongoose=require('mongoose');
+const {graphql} = require('graphql');
+const schema = require('./schema.js');
 mongoose.connect('mongodb://test:tracking@ds054289.mlab.com:54289/tracking');
 module.exports = function(context, req) {
+
   graphql(schema, req.body)
     .then((result) => {
-      res = rsult;
+       context.log(result);
+      res = result;
     });
     context.done(null, res);
 };
